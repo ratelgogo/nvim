80f65abf2d851ca2128bd96e96692a3e1b44102b
zhangruofan <zrf@codeboxes.cn>
2023-12-13 23:37:16 +0800

    dap

--- plugins/dap.lua
+++ plugins/dap.lua
@@ -1,4 +1,4 @@
--- local dap = require('dap')
+local dap = require "dap"
 -- local function get_spring_boot_runner(profile, debug)
 --   local debug_param = ""
 --   if debug then
@@ -19,33 +19,42 @@
 -- -- vim.keymap.set("n", "<F9>", function() run_spring_boot() end)
 -- -- vim.keymap.set("n", "<leader>dS", function() run_spring_boot(true) end)
 --
--- dap.adapters.java = function (callback, config, parent)
---   callback({
---     type = 'server',
---     host = '127.0.0.1';
---     port = 5005;
---   })
--- end
---   dap.configurations.java = {
---     {
---       type = 'java';
---       request = 'attach';
---       name = "Attach to the process";
---       hostName = '127.0.0.1';
---       port = 5005;
---     },
---   }
---
---   dap.configurations.groovy = {
---     {
---       type = 'java';
---       request = 'attach';
---       name = "Attach to the process";
---       hostName = '127.0.0.1';
---       port = 5005;
---     },
---   }
---
-return {
 
+function attach_to_debug()
+  dap.adapters.java = function(callback)
+  -- Here a function needs to trigger the `vscode.java.startDebugSession` LSP command
+  -- The response to the command must be the `port` used below
+  callback({
+    type = 'server';
+    host = '127.0.0.1';
+    port = 5005;
+  })
+end
+  dap.configurations.java = {
+    {
+      type = "java",
+      request = "attach",
+      name = "Attach to the process",
+      hostName = "127.0.0.1",
+      port = 5005,
+    },
+  }
+  dap.continue()
+end
+
+vim.keymap.set("n", "<leader>da", ":lua attach_to_debug()<CR>")
+local jdtls = require("jdtls")
+
+local java_debug_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
+local config = {
+  init_options = {
+    bundles = {
+      vim.fn.glob(java_debug_path .. "com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"),
+    },
+  },
+  on_attach = function() jdtls.setup_dap { hotcodereplace = "auto" } end,
 }
+
+jdtls.start_or_attach(config)
+
+return {}
