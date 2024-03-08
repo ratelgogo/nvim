0a81f96c4bb6bf9fafb73c3863544b11aedf8aa6
zhangruofan <zhangruofan@duiba.com.cn>
2024-03-08 09:53:31 +0800

    modify theme

--- init.lua
+++ init.lua
@@ -57,8 +57,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "onelight",
-  -- colorscheme = "onedark_vivid",
+  -- colorscheme = "onelight",
+  colorscheme = "onedark_vivid",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
